794f962ddfb919ae698837c7dd2be5f0c8867732
caihg <1175867101@qq.com>
2019-10-08 15:24:04 +0800

    使用httpClient，发送带参数的post请求

--- spider/src/main/java/com/acai/just4fun/spider/CrawlerService.java
+++ spider/src/main/java/com/acai/just4fun/spider/CrawlerService.java
@@ -1,35 +1,46 @@
 package com.acai.just4fun.spider;
 
-import java.io.BufferedReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CrawlerService {
 
     public void crawl(String urlString) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(urlString);
+        httpPost.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0");
+        List<NameValuePair> nvps = new ArrayList<>();
+        nvps.add(new BasicNameValuePair("page", "1"));
+        nvps.add(new BasicNameValuePair("city", "101020100"));
+        nvps.add(new BasicNameValuePair("query", "java"));
+        httpPost.setEntity(new UrlEncodedFormEntity(nvps));
 
-        connection.setRequestProperty("accept", "*/*");
-        connection.setRequestProperty("connection", "Keep-Alive");
-        connection.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36");
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(httpPost);
 
-        StringBuilder result = new StringBuilder();
-        connection.connect();
-        InputStream in = connection.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        String line;
-        while ((line = br.readLine()) != null) {
-            result.append(line);
+            if (HttpStatus.SC_OK == response.getStatusLine().getStatusCode()) {
+                System.out.println(EntityUtils.toString(response.getEntity()));
+            } else {
+                System.out.println("请求失败");
+            }
+        } finally {
+            if (response != null) {
+                response.close();
+            }
         }
-
-        if (in != null) {
-            in.close();
-        }
-
-        System.out.println(result);
     }
 }
--- spider/src/test/java/TestSpider.java
+++ spider/src/test/java/TestSpider.java
@@ -3,9 +3,14 @@ import com.acai.just4fun.spider.CrawlerService;
 import java.io.IOException;
 
 public class TestSpider {
-    public static void main(String[] args) throws IOException {
-        String urlStr = "https://www.zhihu.com";
+    public static void main(String[] args) {
+        String urlStr = "https://www.zhipin.com/mobile/jobs.json";
         CrawlerService cs = new CrawlerService();
-        cs.crawl(urlStr);
+        try {
+            cs.crawl(urlStr);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("爬取失败,io异常");
+        }
     }
 }
